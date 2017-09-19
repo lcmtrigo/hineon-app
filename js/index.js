d14bdd603e4542af0a9a79917b93d6c1f8cdc857
rmtrigo <rmontrigo@gmail.com>
2017-09-19 16:35:09 -0700

    basic layout and interface

--- js/index.js
+++ js/index.js
@@ -20,8 +20,17 @@ $(document).ready(function()
     
   $('#left').click(function() { Slide('back'); }); 
   $('#right').click(function() { Slide('forward'); }); 
+
+  /*** avail fonts ***/
+
+  // $('box').click(function(){
+  //   if($('.font-active').length){
+  //     $('.font-active').not($(this)).removeClass('font-active').addClass('box');
+  //   }
+  //   $(this).removeClass('box').addClass('font-active');
+  // });
         
-  });
+});
 
 function Slide(direction)
 	{
@@ -52,3 +61,38 @@ function DoIt(target)
   $('#count').html($.global.item+1);
     
   }
+
+/*** bottom menu ***/
+
+function openMenu(evt, menuName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(menuName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+$('.box').on('click', function(){
+    if ($('.chosenFont').length) {
+      $('.chosenFont').not($(this)).removeClass('chosenFont').addClass('box');
+    };
+    $(this).removeClass('chosenFont').addClass('box');
+  });
+
+// todo: remove bottom border when tabcontent is closed
+// function menuClose() {
+//   document.getElementById("menu").style.display='none';
+// }
