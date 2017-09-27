24bf5eb8c1870075a2458e855c6276742a40e59e
claraaugust <claraaugust@users.noreply.github.com>
2017-09-27 14:53:57 -0700

    Add files via upload

---
+++ qtipsizecolor.js
@@ -0,0 +1,308 @@
+
+/* size & color */
+
+$('.qtipSmall').qtip({
+    content: '0" /line',
+    position: {
+        my: 'top center',  // Position my top left...
+        at: 'bottom center', // at the bottom right of...
+        target: $('.qtipSmall') // my target
+    },
+    show: {
+        event: 'click mouseenter'
+    },
+    hide: {
+        event: 'unfocus'
+    },
+    style: {
+        classes: 'qtip-dark'
+    }
+});
+
+$('.qtipMedium').qtip({
+    content: '0" /line',
+    position: {
+        my: 'top center',  // Position my top left...
+        at: 'bottom center', // at the bottom right of...
+        target: $('.qtipMedium') // my target
+    },
+    show: {
+        event: 'click mouseenter'
+    },
+    hide: {
+        event: 'unfocus'
+    },
+    style: {
+        classes: 'qtip-dark'
+    }
+});
+
+$('.qtipLarge').qtip({
+    content: '0" /line',
+    position: {
+        my: 'top center',  // Position my top left...
+        at: 'bottom center', // at the bottom right of...
+        target: $('.qtipLarge') // my target
+    },
+    show: {
+        event: 'click mouseenter'
+    },
+    hide: {
+        event: 'unfocus'
+    },
+    style: {
+        classes: 'qtip-dark'
+    }
+});
+
+$('.qtipCw').qtip({
+    content: 'Cool White',
+    position: {
+        my: 'top center',  // Position my top left...
+        at: 'bottom center', // at the bottom right of...
+        target: $('.qtipCw') // my target
+    },
+    show: {
+        event: 'click mouseenter'
+    },
+    hide: {
+        event: 'unfocus'
+    },
+    style: {
+        classes: 'qtip-dark'
+    }
+});
+
+$('.qtipWw').qtip({
+    content: 'Warm White',
+    position: {
+        my: 'top center',  // Position my top left...
+        at: 'bottom center', // at the bottom right of...
+        target: $('.qtipWw') // my target
+    },
+    show: {
+        event: 'click mouseenter'
+    },
+    hide: {
+        event: 'unfocus'
+    },
+    style: {
+        classes: 'qtip-dark'
+    }
+});
+
+$('.qtipWr').qtip({
+    content: 'Warm Red',
+    position: {
+        my: 'top center',  // Position my top left...
+        at: 'bottom center', // at the bottom right of...
+        target: $('.qtipWr') // my target
+    },
+    show: {
+        event: 'click mouseenter'
+    },
+    hide: {
+        event: 'unfocus'
+    },
+    style: {
+        classes: 'qtip-dark'
+    }
+});
+
+$('.qtipWo').qtip({
+    content: 'Warm Orange',
+    position: {
+        my: 'top center',  // Position my top left...
+        at: 'bottom center', // at the bottom right of...
+        target: $('.qtipWo') // my target
+    },
+    show: {
+        event: 'click mouseenter'
+    },
+    hide: {
+        event: 'unfocus'
+    },
+    style: {
+        classes: 'qtip-dark'
+    }
+});
+
+$('.qtipWg').qtip({
+    content: 'Warm Green',
+    position: {
+        my: 'top center',  // Position my top left...
+        at: 'bottom center', // at the bottom right of...
+        target: $('.qtipWg') // my target
+    },
+    show: {
+        event: 'click mouseenter'
+    },
+    hide: {
+        event: 'unfocus'
+    },
+    style: {
+        classes: 'qtip-dark'
+    }
+});
+
+$('.qtipWb').qtip({
+    content: 'Warm Blue',
+    position: {
+        my: 'top center',  // Position my top left...
+        at: 'bottom center', // at the bottom right of...
+        target: $('.qtipWb') // my target
+    },
+    show: {
+        event: 'click mouseenter'
+    },
+    hide: {
+        event: 'unfocus'
+    },
+    style: {
+        classes: 'qtip-dark'
+    }
+});
+
+$('.qtipWp').qtip({
+    content: 'Warm Pink',
+    position: {
+        my: 'top center',  // Position my top left...
+        at: 'bottom center', // at the bottom right of...
+        target: $('.qtipWp') // my target
+    },
+    show: {
+        event: 'click mouseenter'
+    },
+    hide: {
+        event: 'unfocus'
+    },
+    style: {
+        classes: 'qtip-dark'
+    }
+});
+
+$('.qtipCr').qtip({
+    content: 'Cool Red',
+    position: {
+        my: 'top center',  // Position my top left...
+        at: 'bottom center', // at the bottom right of...
+        target: $('.qtipCr') // my target
+    },
+    show: {
+        event: 'click mouseenter'
+    },
+    hide: {
+        event: 'unfocus'
+    },
+    style: {
+        classes: 'qtip-dark'
+    }
+});
+
+$('.qtipCo').qtip({
+    content: 'Cool Orange',
+    position: {
+        my: 'top center',  // Position my top left...
+        at: 'bottom center', // at the bottom right of...
+        target: $('.qtipCo') // my target
+    },
+    show: {
+        event: 'click mouseenter'
+    },
+    hide: {
+        event: 'unfocus'
+    },
+    style: {
+        classes: 'qtip-dark'
+    }
+});
+
+$('.qtipCy').qtip({
+    content: 'Cool Yellow',
+    position: {
+        my: 'top center',  // Position my top left...
+        at: 'bottom center', // at the bottom right of...
+        target: $('.qtipCy') // my target
+    },
+    show: {
+        event: 'click mouseenter'
+    },
+    hide: {
+        event: 'unfocus'
+    },
+    style: {
+        classes: 'qtip-dark'
+    }
+});
+
+$('.qtipCly').qtip({
+    content: 'Cool Light Yellow',
+    position: {
+        my: 'top center',  // Position my top left...
+        at: 'bottom center', // at the bottom right of...
+        target: $('.qtipCly') // my target
+    },
+    show: {
+        event: 'click mouseenter'
+    },
+    hide: {
+        event: 'unfocus'
+    },
+    style: {
+        classes: 'qtip-dark'
+    }
+});
+
+$('.qtipCg').qtip({
+    content: 'Cool Green',
+    position: {
+        my: 'top center',  // Position my top left...
+        at: 'bottom center', // at the bottom right of...
+        target: $('.qtipCg') // my target
+    },
+    show: {
+        event: 'click mouseenter'
+    },
+    hide: {
+        event: 'unfocus'
+    },
+    style: {
+        classes: 'qtip-dark'
+    }
+});
+
+$('.qtipCb').qtip({
+    content: 'Cool Blue',
+    position: {
+        my: 'top center',  // Position my top left...
+        at: 'bottom center', // at the bottom right of...
+        target: $('.qtipCb') // my target
+    },
+    show: {
+        event: 'click mouseenter'
+    },
+    hide: {
+        event: 'unfocus'
+    },
+    style: {
+        classes: 'qtip-dark'
+    }
+});
+
+$('.qtipCp').qtip({
+    content: 'Cool Pink',
+    position: {
+        my: 'top center',  // Position my top left...
+        at: 'bottom center', // at the bottom right of...
+        target: $('.qtipCp') // my target
+    },
+    show: {
+        event: 'click mouseenter'
+    },
+    hide: {
+        event: 'unfocus'
+    },
+    style: {
+        classes: 'qtip-dark'
+    }
+});
